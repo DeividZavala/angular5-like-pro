395ade0183411e6029c3f0964bba8036b47870af
DeividZavala <crusedmoss@gmail.com>
2018-03-01 14:14:26 -0600

    inyectando contectos al templa

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {
-  AfterContentInit, Component, ComponentFactoryResolver, ComponentRef, ViewChild,
+  AfterContentInit, Component, ComponentFactoryResolver, ComponentRef, TemplateRef, ViewChild,
   ViewContainerRef
 } from '@angular/core';
 
@@ -11,41 +11,23 @@ import { User } from './auth-form/auth-form.interface';
   selector: 'app-root',
   template: `
     <div>
-      <button (click)="destroyComponent()">
-        Destroy
-      </button>
-      <button (click)="moveComponent()">
-        Move
-      </button>
       <div #entry></div>
+      <template #tmpl let-name let-location="location">
+        {{name}}: {{location}}
+      </template>
     </div>
   `
 })
 export class AppComponent implements AfterContentInit{
 
-  component: ComponentRef<AuthFormComponent>;
-
-  constructor(
-    private resolver: ComponentFactoryResolver
-  ){}
-
   @ViewChild('entry', {read: ViewContainerRef }) entry: ViewContainerRef;
+  @ViewChild('tmpl') tmpl : TemplateRef<any>;
 
   ngAfterContentInit(){
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.entry.createComponent(authFormFactory);
-    this.component = this.entry.createComponent(authFormFactory, 0);
-    // Una vez que creamos el componente podemos cambiar sus atributos
-    this.component.instance.title = "Crear cuenta";
-    this.component.instance.submitted.subscribe(this.loginUser);
-  }
-
-  destroyComponent(){
-    this.component.destroy();
-  }
-
-  moveComponent(){
-    this.entry.move(this.component.hostView, 1);
+    this.entry.createEmbeddedView(this.tmpl,{
+      $implicit: "David Zavala",
+      location: "Mexico City, MX"
+    });
   }
 
   loginUser(user: User) {
