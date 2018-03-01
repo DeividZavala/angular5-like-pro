a9a84226227f4736020932598f64129650e0cc1c
DeividZavala <crusedmoss@gmail.com>
2018-03-01 14:50:04 -0600

    ChangeDetectionStrategy :heart:

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,55 @@
-import {
-  AfterContentInit, Component, ComponentFactoryResolver, ComponentRef, TemplateRef, ViewChild,
-  ViewContainerRef
-} from '@angular/core';
+import { Component,ChangeDetectionStrategy } from '@angular/core';
 
-import {AuthFormComponent} from "./auth-form/auth-form.component";
-
-import { User } from './auth-form/auth-form.interface';
+/**
+ * El change detection strategy es la razon de que angular pueda ser más veloz
+ *
+ * Defaul:  compara cada una de las popiedades el objeto y si alguna cambia, angular actualiza
+ *
+ * onPush: Compara el objeto o modelo completo y solo es actualizado si este es modificado por completo
+ *
+ * Esto permite que las aplicaciones sean mas rapidas ya que angular no tiene que evaluar propiedad por propiedas
+ * si no el objeto completo.
+ *
+ */
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.Default,
+  //changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div>
-      <div #entry></div>
-      <template #tmpl let-name let-location="location">
-        {{name}}: {{location}}
-      </template>
+      <button (click)="addProp()">Add property</button>
+      <button (click)="changeUser()">Change user object</button>
+      <button (click)="changeName()">Change name property</button>
+      <div class="users">
+        <example-one [user]="user"></example-one>
+        <example-two [user]="user"></example-two>
+      </div>
     </div>
   `
 })
-export class AppComponent implements AfterContentInit{
+export class AppComponent{
 
-  @ViewChild('entry', {read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl : TemplateRef<any>;
+  user: any={
+    name: "David Zavala",
+    age: 20,
+    location: "Mexico"
+  };
 
-  ngAfterContentInit(){
-    this.entry.createEmbeddedView(this.tmpl,{
-      $implicit: "David Zavala",
-      location: "Mexico City, MX"
-    });
+  addProp(){
+    this.user.email = "pedrito@sola.com"
   }
 
-  loginUser(user: User) {
-    console.log('Login', user);
+  changeName(){
+    this.user.name = "Elza nahoria";
+  }
+
+  changeUser(){
+    this.user = {
+      name: "Pollito Velazquez",
+      age: 20,
+      location: "Neza"
+    }
   }
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AuthFormModule } from './auth-form/auth-form.module';
 
 import { AppComponent } from './app.component';
+import {ExampleOneComponent} from "./one/one.component";
+import {ExampleTwoComponent} from "./two/two.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleOneComponent,
+    ExampleTwoComponent
   ],
   imports: [
-    BrowserModule,
-    AuthFormModule
+    BrowserModule
   ],
   bootstrap: [
     AppComponent
