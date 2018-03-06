5fdec34a57a3def8e42742b4a99a713cf40458df
DeividZavala <crusedmoss@gmail.com>
2018-03-06 12:49:15 -0600

    reactiveform setup

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {FormControl, FormGroup, FormArray} from "@angular/forms";
 
 @Component({
   selector: 'stock-inventory',
@@ -6,9 +7,51 @@ import {Component} from "@angular/core";
   template: `
   
     <div class="stock-inventory">
-      hola morros
+      <form [formGroup]="form" (submit)="onSubmit()">
+        
+        <stock-branch
+          [parent]="form">
+        </stock-branch>
+        
+        <stock-selector 
+          [parent]="form">
+        </stock-selector>
+        
+        <stock-products 
+          [parent]="form">
+        </stock-products>
+        
+        <div class="stock-inventory__buttons">
+          <button 
+            type="submit"
+            [disabled]="form.invalid" >
+            Order Item
+          </button>
+        </div>
+        
+        <pre>{{form.value | json}}</pre>
+        
+      </form>
     </div>
   
   `
 })
-export class StockInventoryComponent{}
+export class StockInventoryComponent{
+
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl(''),
+      code: new FormControl('')
+    }),
+    selector: new FormGroup({
+      products_id: new FormControl(''),
+      quantity:  new FormControl(10)
+    }),
+    stock: new FormArray([])
+  });
+
+  onSubmit(){
+    console.log("Form", this.form.value);
+  }
+
+}
